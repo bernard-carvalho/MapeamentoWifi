1045f59ff73ba7fa541644b8be4c19b41e897a2c
bernard-carvalho <bernard.carvalho@ifto.edu.br>
2022-09-21 12:06:32 -0300

    criada guard clause para impedir a leitura de linhas vazias

--- EduroamLogToJson/process.php
+++ EduroamLogToJson/process.php
@@ -2,9 +2,10 @@
     echo "[";
     $file_name = "../tmp/logfile";
     $cat_file_command = "cat ".$file_name;
-    exec($cat_file,$conteudo_arquivo,$return);
+    exec($cat_file_command,$conteudo_arquivo,$return);
     for($linha_arquivo=0;$linha_arquivo<count($conteudo_arquivo);$linha_arquivo++){
-        
+        if(strlen($conteudo_arquivo[$linha_arquivo])==0)
+            break;
     }
-    echo "]"
+    echo "]";
 ?>
