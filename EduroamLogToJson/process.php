3da51b31a29da316ad6c9c9f768a37e6a415072a
bernard-carvalho <bernard.carvalho@ifto.edu.br>
2022-09-21 12:10:58 -0300

    criada condicional para identificar se a entrada não for a primeira, fechando a entrada anterior

--- EduroamLogToJson/process.php
+++ EduroamLogToJson/process.php
@@ -3,11 +3,13 @@
     $file_name = "../tmp/logfile";
     $cat_file_command = "cat ".$file_name;
     exec($cat_file_command,$conteudo_arquivo,$return);
+    $number_of_entry=0;
     for($linha_arquivo=0;$linha_arquivo<count($conteudo_arquivo);$linha_arquivo++){
         if(strlen($conteudo_arquivo[$linha_arquivo])==0)
             continue;
         if($conteudo_arquivo[$linha_arquivo][0]!="\t"){
-            
+            if($number_of_entry!=0)
+                echo "}, \n";
         }
 
     }
