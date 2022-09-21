c314b31e177191957370ca5ed4b86e5d1cf475cd
bernard-carvalho <bernard.carvalho@ifto.edu.br>
2022-09-21 12:08:26 -0300

    Criada condicional para identificar novas entradas

--- EduroamLogToJson/process.php
+++ EduroamLogToJson/process.php
@@ -5,7 +5,11 @@
     exec($cat_file_command,$conteudo_arquivo,$return);
     for($linha_arquivo=0;$linha_arquivo<count($conteudo_arquivo);$linha_arquivo++){
         if(strlen($conteudo_arquivo[$linha_arquivo])==0)
-            break;
+            continue;
+        if($conteudo_arquivo[$linha_arquivo][0]!="\t"){
+            
+        }
+
     }
     echo "]";
 ?>
