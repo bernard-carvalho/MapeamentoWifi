8946960e7ac5eac2f01d25734cd62963fec6025e
bernard-carvalho <bernard.carvalho@ifto.edu.br>
2022-09-28 14:02:59 -0300

    update

--- heatmap.php
+++ heatmap.php
@@ -4,15 +4,23 @@
             echo "Failed to connect to MySQL ".mysqli_connect_error();
             exit();
     }
-    $query =" SELECT * FROM MAC_STA INNER JOIN AP ON MAC_STA.mac_ap=AP.ap_mac WHERE ".
-            "MAC_STA.date>=\"".$busca_data_inicio."\" ".
-            " AND ".
-            "MAC_STA.date<=\"".$busca_data_fim."\" ".
-            " AND ".
-            "MAC_STA.ip_sta=\"".$busca_ip."\" ".
-            "ORDER BY date";
+    $query = "call heatmap()";
     if(!($result = $conexao_mysql -> query($query))){
             echo "Failed to query MySQL";
             exit();
     }
+    $resultados=array();
+    for($i=0; $i<$result->num_rows;$i++){//itera entre cada linha da resposta
+        $row = $result->fetch_array();
+        //echo $row["id"]."\n";
+        $resultado = "{".
+                            "\""."id_bloco"."\"".      ":"   ."\"".$row["id"]."\""    . ",".
+                            "\""."nome_bloco"."\"".      ":"   ."\"".$row["bloco"]."\""    . ",".
+                            "\""."qntd"."\"".      ":"   ."\"".$row["quantidade_clientes"]."\"".
+                        "}";
+        //echo $resultado."\n";
+        $resultados[] = json_decode($resultado);
+        //var_dump($resultados);
+    }
+    echo json_encode($resultados);
 ?>
