b10dac56e22d58c8c6d35f51fe4300d590870de1
bernard-carvalho <bernard.carvalho@ifto.edu.br>
2022-09-29 12:54:27 -0300

    atualizacoes na interface

--- heatmap.php
+++ heatmap.php
@@ -10,17 +10,23 @@
             exit();
     }
     $resultados=array();
+    $quantidade_total_clientes=0;
     for($i=0; $i<$result->num_rows;$i++){//itera entre cada linha da resposta
         $row = $result->fetch_array();
         //echo $row["id"]."\n";
         $resultado = "{".
                             "\""."id_bloco"."\"".      ":"   ."\"".$row["id"]."\""    . ",".
                             "\""."nome_bloco"."\"".      ":"   ."\"".$row["bloco"]."\""    . ",".
-                            "\""."qntd"."\"".      ":"   ."\"".$row["quantidade_clientes"]."\"".
+                            "\""."qntd"."\"".      ":"   ."\"".$row["quantidade_clientes"]."\"".",".
+                            "\""."X"."\"".      ":"   ."\"".$row["posX"]."\"".",".
+                            "\""."Y"."\"".      ":"   ."\"".$row["posY"]."\"".
                         "}";
+        $quantidade_total_clientes+=intval($row["quantidade_clientes"]);
         //echo $resultado."\n";
         $resultados[] = json_decode($resultado);
         //var_dump($resultados);
     }
-    echo json_encode($resultados);
+        $last_element="{"."\"quantidadeTotalClientes\":"."\"".$quantidade_total_clientes."\""."}";
+        $resultados[] = json_decode($last_element);
+        echo json_encode($resultados);
 ?>
